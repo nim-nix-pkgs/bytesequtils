b6dcb53e82198d1dd903132ee6676ac7762f9ed2
hugosenari <hugosenari@gmail.com>
2022-03-12 00:57:55 -0300

    chore: re index bytesequtils

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = ''Nim package to manipulate buffer as either seq[byte] or string'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."bytesequtils-main".url = "path:./main";
+  inputs."bytesequtils-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bytesequtils-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."bytesequtils-v1_0_0".url = "path:./v1_0_0";
+  inputs."bytesequtils-v1_0_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bytesequtils-v1_0_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."bytesequtils-v1_1_0".url = "path:./v1_1_0";
+  inputs."bytesequtils-v1_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bytesequtils-v1_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."bytesequtils-v1_1_1".url = "path:./v1_1_1";
+  inputs."bytesequtils-v1_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bytesequtils-v1_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."bytesequtils-v1_1_2".url = "path:./v1_1_2";
+  inputs."bytesequtils-v1_1_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bytesequtils-v1_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."bytesequtils-v1_1_3".url = "path:./v1_1_3";
+  inputs."bytesequtils-v1_1_3".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bytesequtils-v1_1_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."bytesequtils-v1_2_0".url = "path:./v1_2_0";
+  inputs."bytesequtils-v1_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bytesequtils-v1_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ v1_1_1/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Nim package to manipulate buffer as either seq[byte] or string'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-bytesequtils-v1_1_1.flake = false;
+  inputs.src-bytesequtils-v1_1_1.owner = "Clonkk";
+  inputs.src-bytesequtils-v1_1_1.ref   = "refs/tags/v1.1.1";
+  inputs.src-bytesequtils-v1_1_1.repo  = "bytesequtils";
+  inputs.src-bytesequtils-v1_1_1.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-bytesequtils-v1_1_1"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-bytesequtils-v1_1_1";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
